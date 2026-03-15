c5a7266828e15980bd15052fefd3a587585eb786
Ashutosh Chauhan <ashutoshchauhan8468@gmail.com>
2026-03-15 10:04:15 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","web Developre","Youtuber","Video Editor"],
+    strings:["","Web Designer","web Developer","Youtuber","Video Editor"],
     typeSpeed:100,
     backSpeed:60,
     loop:true
